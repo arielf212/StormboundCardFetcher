7299342a23650fd8cbdb21c911105e88f6a1c053
ariel <ariel.f2002@gmail.com>
2018-07-02 01:17:18 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,37 @@
+import csv
+import difflib
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='?') # bot creation
+
+def get_card_name(text):
+    '''takes a string and extracts card names from it. card names are encapsulated in [[xxxx]] where xxxx is the card name'''
+    cards = [] # list of names of cards
+    start = text.find('[[')
+    while start != -1: # until there are no more brackets
+        end = text.find(']]')
+        if end == -1:
+            return cards # if there is an opener but no closer then someone fucked up
+        else:
+            cards.append(text[start+2:end]) # gets the name of the card
+        text = text[end+2 : ] # cuts out the part with the card
+        start = text.find('[[') # and the circle begins anew
+    return cards
+
+def load_cards():
+    cards = {}
+    with open('card_list' , 'r') as fcard_list:
+        card_list = csv.reader(fcard_list , delimiter = '%')
+        for row in card_list:
+            name , link = row
+            cards[name] = link
+    return cards
+@bot.event
+async def on_message(message):
+    if message.content.find('[[') != -1:
+        cards = get_card_name(message.content) # get the card name out of the message
+        for card in cards:
+            await bot.send_message(message.channel , card_list[difflib.get_close_matches(card , card_list , n = 1)[0]])
+#main
+card_list = load_cards()
+bot.run('NDYzMDk1MjAzMzY1MTI2MTU0.Dhre2A.Q9kY09phR10E6nTr1T0o47foDjY')
